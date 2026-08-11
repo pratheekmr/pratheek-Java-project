85d6b66824cdc47b98aedb5ef00ff92a0e1fbf32
Ubuntu <ubuntu@ip-172-31-18-130.ec2.internal>
2026-08-11 12:36:28 +0000

    Test automatic Jenkins trigger

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -15,7 +15,7 @@ public class Calculator {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
 
-        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Addition Result: " + calc.add(10, 5));
         System.out.println("Subtraction: " + calc.subtract(10, 5));
         System.out.println("Multiplication: " + calc.multiply(10, 5));
     }
